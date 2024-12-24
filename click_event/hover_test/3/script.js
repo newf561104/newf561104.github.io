a8938c9bc647a1d40a6d476564ac71f486ade33e
中野隆行 <nakano_takayuki@uniquevision.co.jp>
2024-12-24 17:51:44 +0900

    click だけ監視

--- click_event/hover_test/3/script.js
+++ click_event/hover_test/3/script.js
@@ -5,18 +5,18 @@ $(function() {
     el_eventname.append(eventname);
   };
 
-  $(document).on('touchstart', 'div', function () {
-    updateEventname('<p>touchstart</p>')
-  });
-  $(document).on('touchend', 'div', function () {
-    updateEventname('<p>touchend</p>')
-  });
-  $(document).on('mousedown', 'div', function () {
-    updateEventname('<p>mousedown</p>')
-  });
-  $(document).on('mouseup', 'div', function () {
-    updateEventname('<p>mouseup</p>')
-  });
+  // $(document).on('touchstart', 'div', function () {
+  //   updateEventname('<p>touchstart</p>')
+  // });
+  // $(document).on('touchend', 'div', function () {
+  //   updateEventname('<p>touchend</p>')
+  // });
+  // $(document).on('mousedown', 'div', function () {
+  //   updateEventname('<p>mousedown</p>')
+  // });
+  // $(document).on('mouseup', 'div', function () {
+  //   updateEventname('<p>mouseup</p>')
+  // });
   $(document).on('click', 'div', function () {
     updateEventname('<p>click</p>')
   });
