5949e4a5af72585083ef5ff9a8dc152384b2b5ed
中野隆行 <nakano_takayuki@uniquevision.co.jp>
2024-12-24 16:50:31 +0900

    event listen

---
+++ click_event/hover_test/script.js
@@ -0,0 +1,30 @@
+
+let hover_element = document.getElementById('hov_elm');
+let el_eventname = document.getElementById('event_name');
+
+// 表示をアップデートする関数群
+let updateEventname = function(eventname) {
+  el_eventname.innerHTML = `${el_eventname.innerHTML} ${eventname}`;
+};
+
+// イベント設定
+hover_element.addEventListener('touchstart', function(event) {
+  updateEventname('touchstart');
+}, false);
+
+hover_element.addEventListener('touchend', function(event) {
+  updateEventname('touchend')
+}, false);
+
+hover_element.addEventListener('mousedown', function(event) {
+  updateEventname('mousedown')
+}, false);
+
+hover_element.addEventListener('mouseup', function(event) {
+  updateEventname('mouseup')
+}, false);
+
+hover_element.addEventListener('click', function(event) {
+  updateEventname('click')
+}, false);
+
