5f898b6e9040eac22ae731d995ca3b27b61ddad8
中野隆行 <nakano_takayuki@uniquevision.co.jp>
2024-12-25 12:24:53 +0900

    coordinate

--- click_event/hover_listen2/script.js
+++ click_event/hover_listen2/script.js
@@ -7,24 +7,28 @@ let updateEventname = function(eventname) {
   el_eventname.innerHTML = `${el_eventname.innerHTML}<p>${eventname}</p>`;
 };
 
+let updateTouchEvent = function(eventname, event) {
+  innerHTML = `${el_eventname.innerHTML}<p>${eventname}: ${event.changedTouches[0].pageX}, ${event.changedTouches[0].pageY} </p>`;
+}
+
 // イベント設定
 hover_element.addEventListener('touchstart', function(event) {
-  updateEventname('touchstart');
+  updateTouchEvent('touchstart', event);
 }, false);
 
 hover_element.addEventListener('touchend', function(event) {
-  updateEventname('touchend')
+  updateTouchEvent('touchend', event);
 }, false);
 
 hover_element.addEventListener('mousedown', function(event) {
-  updateEventname('mousedown')
+  updateEventname('mousedown');
 }, false);
 
 hover_element.addEventListener('mouseup', function(event) {
-  updateEventname('mouseup')
+  updateEventname('mouseup');
 }, false);
 
 hover_element.addEventListener('click', function(event) {
-  updateEventname('click')
+  updateEventname('click');
 }, false);
 
